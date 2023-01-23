0805768d7253f63474e4c30b19bfe891faefb78e
kelvin prinz <kelvinprinzossai@gmail.com>
2023-01-23 12:19:51 +0100

    booking

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -31,9 +31,9 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('home')}}">
                 <div class="sidebar-brand-icon">
-                    <div><a href="{{route('home')}}">StayInn</a></div>
+                    <div>StayInn</div>
                 </div>
             </a>
 
